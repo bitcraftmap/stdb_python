320e9c803de792fae00c7869226a7f3e84f0798d
bitcraftmap <bitcraftmap@emailproxy.org>
2025-08-16 18:07:30 +0200

    cleaned code

--- slash_region_data.py
+++ slash_region_data.py
@@ -1,42 +1,50 @@
-# Goal is to reduce chunk data to the minimum
-
-'''
-'chunk_index': 196132,
-'chunk_x''"': 131,
-'chunk_z': 196,
-'dimension': 1,
-'biomes': [1,1,1,1,1,1,1,1,1,1
-'elevations': [25,25,25,25,25,
-'water_levels': [0,0,0,0,0,0,0
-'water_body_types': [0,0,0,0,0
-'zoning_types': [0,0,0,0,0,0,0
-'original_elevations': [25,25,
-'''
-
-# remove : original_elevations, elevations, zoning_types, water_levels,
-# remove all 0 0 chunks ? real 0 0 will be missing
-
 import json
 
-chunk_data_file = 'assets/data/terrain_chunk_state/region_3/terrain_chunk_state.json'
-chunk_data_file_new = 'assets/data/terrain_chunk_state/region_3/terrain_chunk_state_new.json'
+#This script will take the chunk files and remove some key to reduce the size of the json file
 
-remove_keys = ['elevations', 'water_levels', 'zoning_types', 'original_elevations', 'chunk_index', 'dimension']
+'''
+{
+    'chunk_index': 196132,
+    'chunk_x': 131,
+    'chunk_z': 196,
+    'dimension': 1,
+    'biomes': array[],
+    'elevations': array[],
+    'water_levels': array[],
+    'water_body_types': array[],
+    'zoning_types': array[],
+    'original_elevations': array[],
+}
 
-def remove_keys_from_dict(d, keys_to_remove):
-    if isinstance(d, dict):
+'''
+
+chunk_data_file = 'data/terrain_chunk_state/region_3/terrain_chunk_state.json'
+chunk_data_file_new = 'data/terrain_chunk_state/region_3/terrain_chunk_state_new.json'
+
+remove_keys = [
+    'dimension',
+    'elevations',
+    'water_levels',
+    'water_body_types',
+    'zoning_types',
+    'original_elevations'
+]
+
+def remove_keys_from_dict(data, keys_to_remove):
+    if isinstance(data, dict):
         return {k: remove_keys_from_dict(v, keys_to_remove) 
-                for k, v in d.items() if k not in keys_to_remove}
-    elif isinstance(d, list):
-        return [remove_keys_from_dict(item, keys_to_remove) for item in d]
+                for k, v in data.items() if k not in keys_to_remove}
+    elif isinstance(data, list):
+        return [remove_keys_from_dict(item, keys_to_remove) for item in data]
     else:
-        return d
-
+        return data
 
 with open(chunk_data_file, 'r') as file:
     data = json.load(file)
 
-new_json = remove_keys_from_dict(data, remove_keys)
+filtered_json = [obj for obj in data if obj.get('dimension') == 1]
+new_json = remove_keys_from_dict(filtered_json, remove_keys)
+print(str(len(new_json)) + ' chunks in the array')
 
 with open(chunk_data_file_new, 'w') as file:
-    json.dump(new_json, file)
+    json.dump(new_json, file, separators=(',', ':'))
